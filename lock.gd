1ad0629f29c68c5b995811aeed35231e972d4566
ndrooo <git@ndr.ooo>
2026-08-15 22:07:20 -0400

    Randomize key pin height and add go faster button

--- lock.gd
+++ lock.gd
@@ -6,7 +6,8 @@ const PINS = 5
 
 enum Player {ONE, TWO}
 
-@export var speed = 18.0
+@export var normal_speed = 18.0
+@export var fast_speed = 120.0
 @export var player = Player.ONE
 @export var cross_section = true
 
@@ -14,6 +15,7 @@ var up_action = ""
 var down_action = ""
 var left_action = ""
 var right_action = ""
+var a_action = ""
 var b_action = ""
 
 var active_pin = 0
@@ -25,14 +27,19 @@ func _ready() -> void:
 	down_action = "p1_down" if player == Player.ONE else "p2_down"
 	left_action = "p1_left" if player == Player.ONE else "p2_left"
 	right_action = "p1_right" if player == Player.ONE else "p2_right"
+	a_action = "p1_a" if player == Player.ONE else "p2_a"
 	b_action = "p1_b" if player == Player.ONE else "p2_b"
 	flip = scale.x < 0
 	$Body.visible = !cross_section
+	for pin in $Pins.get_children():
+		var pin_stack: PinStack = pin
+		pin_stack.key_pin_height = randi() % 10 + 10
 
 
 func _process(delta: float) -> void:
 	var pick: CharacterBody2D = $Pick
 	$Pick.velocity = Vector2.ZERO
+	var speed = fast_speed if Input.is_action_pressed(a_action) else normal_speed
 	if Input.is_action_pressed(up_action):
 		pick.position += Vector2.UP * speed * delta
 	if Input.is_action_pressed(down_action):
--- pin_stack.gd
+++ pin_stack.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
+class_name PinStack
+
 @export var binding = false
-@export var key_pin_height = 10
-
-func _ready() -> void:
-	$KeyPin/NinePatchSprite2D.size.y = key_pin_height
-	$KeyPin/Collider.shape.y = key_pin_height + 1
-
-func _process(delta: float) -> void:
-	pass
+@export var key_pin_height = 10:
+	set(new_height):
+		key_pin_height = new_height
+		$KeyPin/NinePatchSprite2D.size.y = key_pin_height
+		$KeyPin/Collider.shape.size.y = key_pin_height + 1
+		$KeyPin/Collider.position.y = key_pin_height / 2.0
