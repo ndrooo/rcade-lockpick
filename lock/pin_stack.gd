62c4b73b4dac0132be936123accd184c8afa82c9
ndrooo <git@ndr.ooo>
2026-08-28 18:28:41 -0400

    First pass at VFX/SFX for pin setting and scraping

--- lock/pin_stack.gd
+++ lock/pin_stack.gd
@@ -3,9 +3,17 @@ extends Node2D
 
 @onready var key: Area2D = $KeyPin
 @onready var driver: Area2D = $DriverPin
+@onready var scraping_sfx = $ScrapingSFX as AudioStreamPlayer2D
+@onready var scraping_sfx_stream = scraping_sfx.stream as AudioStreamGenerator
 
 @export var fall_speed = 80
 @export var shear_height = 25
+@export var scraping_hz = 440.0
+@export var scraping_volume = 0.1
+
+@onready var last_height = driver.position.y
+var scraping = false
+var scraping_audio_phase = 0.0
 
 var binding = false
 var pin_set = false
@@ -33,12 +41,42 @@ func _physics_process(delta: float) -> void:
 	if binding and not pin_set and driver.position.y < -shear_height:
 		binding = false
 		pin_set = true
+		toggle_scraping(false)
+		pin_set_effect()
 		set_pin.emit()
 	if pin_set and driver.position.y > -shear_height:
 		driver.position.y = -shear_height
+	if binding and driver.position.y < last_height and not scraping:
+		toggle_scraping(true)
+	if binding and driver.position.y >= last_height and scraping:
+		toggle_scraping(false)
+	last_height = driver.position.y
+
+func _process(_delta: float) -> void:
+	if scraping_sfx.playing:
+		var increment = scraping_hz / scraping_sfx_stream.mix_rate
+		var playback = scraping_sfx.get_stream_playback()
+		for frame in range(playback.get_frames_available()):
+			# var amplitude = sin(scraping_audio_phase * TAU)
+			var amplitude = randf_range(-0.5, 0.5) * (sin(scraping_audio_phase * TAU) / 2.0)
+			playback.push_frame(Vector2.ONE * amplitude * scraping_volume)
+			scraping_audio_phase = fmod(scraping_audio_phase + increment, 1.0)
 
 func bind_pins():
 	binding = true
+
+func toggle_scraping(is_scraping):
+	scraping = is_scraping
+	$ScrapingVFX.visible = is_scraping
+	if is_scraping:
+		scraping_sfx.play()
+	else:
+		scraping_sfx.stop()
+
+func pin_set_effect():
+	($PinSetSFX as AudioStreamPlayer2D).play(0.25)
+	var vfx = $PinSetVFX as AnimatedSprite2D
+	vfx.play()
 
 func _on_key_pin_body_entered(body: Node2D) -> void:
 	if body is Pick:
